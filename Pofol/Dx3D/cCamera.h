334c4bf429a1b8a487a5d7e734bf19981cd1149b
OasisBong <jadondlek@gmail.com>
2016-12-02 19:28:26 +0900

    다시 업로드

---
+++ Pofol/Dx3D/cCamera.h
@@ -0,0 +1,26 @@
+#pragma once
+class cObjMap;
+class cCamera
+{
+private:
+	SYNTHESIZE(D3DXVECTOR3, m_vEye, Eye);
+
+private:
+	D3DXVECTOR3		m_vLookAt;
+	D3DXVECTOR3		m_vUp;
+	bool			m_isLButtonDown;
+	POINT			m_ptPrevMouse;
+	float			m_fAngleX;
+	float			m_fAngleY;
+	float			m_fDistance;
+
+public:
+	cCamera(void);
+	~cCamera(void);
+
+	void Setup();
+	void Update(D3DXVECTOR3* pTarget = NULL, cObjMap* pMap);
+	//void Update(D3DXVECTOR3* pTarget = NULL, float distance = 0);
+	void WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+};
+
